95c901c590ded683feb2de71dd19be1fc7472a53
Lucie <lucie@Lucies-MacBook-Pro.local>
2017-03-14 20:46:19 +0000

    End of class

--- bank_runner.rb
+++ bank_runner.rb
@@ -15,6 +15,9 @@ while(true)
   puts
   puts "Options:"
   puts " 1: Show Account holders"
+  puts " 2: Show account by type"
+  puts " 3: Show account by risk"
+  puts " 4: Show total balance of bank"
   puts " q: Exit"
 
   choice = gets.chomp().downcase()
@@ -24,5 +27,26 @@ while(true)
   elsif choice == "1"
     puts "Account holders: "
     puts bank_account_owner_names(ACCOUNTS)
+  elsif choice == "2"
+    puts "Choose Business (B) or Personal (P) Account types: "
+      type_choice = gets.chomp().downcase()
+      if type_choice == "b"
+        puts filter_accounts_by_type("business",ACCOUNTS)
+      elsif type_choice == "p"
+        puts filter_accounts_by_type("personal", ACCOUNTS)
+      end
+    elsif choice == "3"
+      puts "What type of risk level would you like? h = High Risk, m = Medium Risk, l = Low Risk"
+      risk_choice = gets.chomp().downcase()
+      if risk_choice == "h"
+        puts find_accounts_by_risk("high", ACCOUNTS)
+      elsif risk_choice == "m"
+        puts find_accounts_by_risk("medium", ACCOUNTS)
+      elsif risk_choice == "l"
+        puts find_accounts_by_risk("low", ACCOUNTS)
+      end
+    elsif choice == "4"
+      puts "The total is: "
+      puts total_cash_in_bank("amount", ACCOUNTS)
   end
 end
